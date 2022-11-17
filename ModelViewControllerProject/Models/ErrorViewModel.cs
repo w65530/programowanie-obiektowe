faa68dca3c41d7bfbf3ed21473330d44e11b0acf
Jakub Chodziński <xapixowy@gmail.com>
2022-11-17 13:20:32 +0100

    New project MVC

---
+++ ModelViewControllerProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModelViewControllerProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
